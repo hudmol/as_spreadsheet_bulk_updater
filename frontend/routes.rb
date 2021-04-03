75cc22c4a3fc70faaa894b60ca0740a01e43b720
Payten Giles <payten.giles@gmail.com>
2021-04-03 16:38:18 +1100

    [Fixes #1] Apply prefix to new frontend routes

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,6 +1,8 @@
 ArchivesSpace::Application.routes.draw do
-
-  match('/plugins/spreadsheet_bulk_updater/download' => 'spreadsheet_bulk_updater#download_form', :via => [:get])
-  match('/plugins/spreadsheet_bulk_updater/download' => 'spreadsheet_bulk_updater#download', :via => [:post])
-
+  [AppConfig[:frontend_proxy_prefix], AppConfig[:frontend_prefix]].uniq.each do |prefix|
+    scope prefix do
+      match('/plugins/spreadsheet_bulk_updater/download' => 'spreadsheet_bulk_updater#download_form', :via => [:get])
+      match('/plugins/spreadsheet_bulk_updater/download' => 'spreadsheet_bulk_updater#download', :via => [:post])
+    end
+  end
 end
