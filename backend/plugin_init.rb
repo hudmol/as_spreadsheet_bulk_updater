06b48c143b0523c361c7d0690093d1817e6c7c47
Payten Giles <payten.giles@gmail.com>
2021-06-23 13:04:24 +1000

    Package xlsx_streaming_reader rather than pulling in as gem

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,3 +1,5 @@
 unless AppConfig[:plugins].include?('digitization_work_order')
   raise "Hi there, thanks for trying out this great plugin! Currently it relies on the digitization_work_order plugin, which you can download here: https://github.com/hudmol/digitization_work_order -- thanks again!"
 end
+
+require_relative '../lib/xlsx_streaming_reader/lib/xlsx_streaming_reader'
